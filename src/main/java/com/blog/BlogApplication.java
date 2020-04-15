0f144c0f954b41e58db9a5d266bedd69bcd69992
lijun <lijun1@zto.com>
2020-04-15 09:53:46 +0800

    引入mapperScan

--- src/main/java/com/blog/BlogApplication.java
+++ src/main/java/com/blog/BlogApplication.java
@@ -1,8 +1,9 @@
 package com.blog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.blog.mapper")
 @SpringBootApplication
 public class BlogApplication {
 
